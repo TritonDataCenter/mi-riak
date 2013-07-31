3693d42949a12e22939d338646260656f4b72506
Derek Crudgington <derek@joyent.com>
2013-07-31 17:11:04 +0000

    Cleaned up the way riak configurations are inserted

--- copy/var/zoneinit/includes/31-riak.sh
+++ copy/var/zoneinit/includes/31-riak.sh
@@ -1,34 +1,18 @@
-BUFFER_POOL_SIZE=$((${RAM_IN_BYTES}/2))
-LOG_BUFFER_SIZE=8388608
-LOG_FILE_SIZE=134217728
-
-log "determining Riak secret cookie"
-
+log "generating riak_secret"
 # First look for the metadata riak_secret key, if set in the request.
-# If not, use special character free password.
-# Worst case, use something hard-coded.
-
-RIAK_SECRET=$(mdata-get riak_secret 2>/dev/null) ||\
-  RIAK_SECRET=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ') ||\
-  RIAK_SECRET=$(echo -n "riak_${ZONENAME}" | /usr/bin/digest -a md5)
+# If not, generate our own.
+RIAK_SECRET=${RIAK_SECRET:-$(mdata-get riak_secret 2>/dev/null) ||\
+RIAK_SECRET=$(od -An -N8 -x /dev/random | head -1 | tr -d ' ');
 
 log "putting real data into Riak config files"
-
-/opt/local/bin/gsed -i""                            \
-  -e "s/##PRIVATE_IP##/${PRIVATE_IP}/"    \
-  -e "s/##BUFFER_POOL_SIZE##/${BUFFER_POOL_SIZE}/" \
-  -e "s/##LOG_BUFFER_SIZE##/${LOG_BUFFER_SIZE}/"   \
-  -e "s/##LOG_FILE_SIZE##/${LOG_FILE_SIZE}/"       \
-  -e "s/##RIAK_SECRET##/${RIAK_SECRET}/"           \
-  /opt/local/etc/riak/{app.config,vm.args}
+gsed -i "s/127.0.0.1/${PRIVATE_IP}/g" /opt/local/etc/riak/app.config /opt/local/etc/riak/vm.args
+gsed -i "s/^-setcookie riak/-setcookie ${RIAK_SECRET}/g" /opt/local/etc/riak/vm.args
 
 log "writing the Erlang cookie file"
-
 echo "${RIAK_SECRET}" > /var/db/riak/.erlang.cookie
 chown riak:riak /var/db/riak/.erlang.cookie
 chmod 400 /var/db/riak/.erlang.cookie
 
 log "starting Riak"
-
 svcadm enable epmd
 svcadm enable riak
